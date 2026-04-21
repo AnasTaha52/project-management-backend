25eef18cb4f6125a036387cdeaaf076849bc55c7
AnasTaha52 <anastaha699@gmail.com>
2026-04-21 13:54:39 +0500

    created seprate app.js file for express server

---
+++ src/app.js
@@ -0,0 +1,33 @@
+import express from "express"
+import cors from "cors"
+
+const app = express()
+
+// Configurations
+
+//  app.use -> middleware
+
+app.use(express.json({limit: "16kb"}))
+app.use(express.urlencoded({extended: true , limit: "16kb"}))
+app.use(express.static("public"))
+
+// cors configurations
+
+app.use(cors({
+    origin: process.env.CORS_ORIGIN?.split(",") || "http://localhost:5173",
+    credentials: true,
+    methods: ["GET" , "POST" , "PUT" , "PATCH" , "DELETE" , "OPTIONS"],
+    allowedHeaders: ["Authorization" , "Content-Type"]
+}))
+
+
+app.get("/" , (req , res) => {
+    res.send("Welcom to basecampy");
+})
+
+app.get("/instagram" , (req , res)=>{
+    res.send("Instagram Page")
+})
+
+
+export default app
