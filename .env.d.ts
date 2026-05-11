0f3339f850d1ce79725ae7914428fb221f5740c0
AnasTaha52 <anastaha699@gmail.com>
2026-05-11 20:35:46 +0500

    del file

--- .env.d.ts
+++
@@ -1,8 +0,0 @@
-declare namespace NodeJS {
-  interface ProcessEnv {
-    PORT?: string;
-    CORS_ORIGIN?: string;
-    MONGO_URI: string;
-    NODE_ENV?: "development" | "production" | "test";
-  }
-}
