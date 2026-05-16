737e83f53271bf66f33b00fbbd863788d023be7c
AnasTaha52 <anastaha699@gmail.com>
2026-05-16 20:28:26 +0500

    added auth for                 forgotPasswordRequest

--- src/controllers/auth.controllers.js
+++ src/controllers/auth.controllers.js
@@ -284,6 +284,41 @@ const refreshAccessToken = asyncHandler(async(req , res) => {
 
 })
 
+const forgotPasswordRequest = asyncHandler(async(req , res) => {
+  const {email} = req.body
+
+  const user =  await User.findOne({email})
+
+  if(!user){
+    throw new ApiError(404, "User does not exists" , []); 
+  }
+
+  const {unHashedToken , hashedToken , tokenExpiry} =  user.generateTemporaryToken()
+
+  user.forgotPasswordToken = hashedToken
+  user.forgotPasswordExpiry = tokenExpiry
+
+  await user.save({validateBeforeSave: false})
+
+  await sendEmail({
+    email: user?.email,
+    subject: "Password Reset Request",
+    mailgenContent: forgotPasswordMailgenContent(
+      user.username,
+      `${process.env.FORGOT_PASSWORD_REDIRECT_URL}/${unHashedToken}`,
+    ),
+  });
+
+  return res.status(200).json(
+    new ApiResponse(
+      200,
+      {},
+      "Password reset mail has been sent to your mail id"
+    )
+  )
+})
+
+
 // const getCurrentUser = asyncHandler(async(req , res) => {})
 
 export {
@@ -294,4 +329,5 @@ export {
   verifyEmail,
   resendEmailVerification,
   refreshAccessToken,
+  forgotPasswordRequest,
 };
