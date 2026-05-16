61cfb235df1de288b08aaa3746710b2bc4f99c1b
AnasTaha52 <anastaha699@gmail.com>
2026-05-16 21:46:18 +0500

    write validators for userChangeCurrentPasswordValidator userForgotPasswordValidator userResetForgotPasswordValidator

--- src/validators/index.js
+++ src/validators/index.js
@@ -1,7 +1,5 @@
 import { body } from "express-validator";
 
-
-
 const userRegisterValidator = () => {
   return [
     body("email")
@@ -18,25 +16,46 @@ const userRegisterValidator = () => {
       .withMessage("Username must be in lower case")
       .isLength({ min: 3 })
       .withMessage("Username must be at least 3 characters long."),
-    body("password")
-      .trim()
-      .notEmpty()
-      .withMessage("password is required"),
-    body("fullname")
-      .optional()
-      .trim()
+    body("password").trim().notEmpty().withMessage("password is required"),
+    body("fullname").optional().trim(),
   ];
 };
 
 const userLoginValidator = () => {
   return [
+    body("email").optional().isEmail().withMessage("Email is Invalid"),
+    body("password").notEmpty().withMessage("passwordis required"),
+  ];
+};
+
+const userChangeCurrentPasswordValidator = () => {
+  return [
+    body("oldPassword").notEmpty().withMessage("password is required"),
+    body("newPassword").notEmpty().withMessage("New password is required"),
+  ];
+};
+
+const userForgotPasswordValidator = () => {
+  return [
     body("email")
-      .optional()
-      .isEmail()
-      .withMessage("Email is Invalid"),
-    body("password")
       .notEmpty()
-      .withMessage("passwordis required")  
-  ]
-}
-export { userRegisterValidator , userLoginValidator};
+      .withMessage("email is required")
+      .isEmail()
+      .withMessage("email is Invalid"),
+  ];
+};
+
+const userResetForgotPasswordValidator = () => {
+  return [
+    body("newPassword")
+      .notEmpty()
+      .withMessage("password is required")
+  ];
+};
+export {
+  userRegisterValidator,
+  userLoginValidator,
+  userChangeCurrentPasswordValidator,
+  userForgotPasswordValidator,
+  userResetForgotPasswordValidator,
+};
