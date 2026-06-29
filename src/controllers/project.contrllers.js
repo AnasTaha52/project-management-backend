9d5b881b238a79715c92f7d945f82484c982f715
AnasTaha52 <anastaha699@gmail.com>
2026-06-29 14:07:23 +0500

    write functionality for getProjectMembers

--- src/controllers/project.contrllers.js
+++ src/controllers/project.contrllers.js
@@ -162,9 +162,63 @@ const addMembersToProject = asyncHandler(async (req, res) => {
 
 });
 
-const getProjectMembers = asyncHandler(async (req, res) => {});
+const getProjectMembers = asyncHandler(async (req, res) => {
+  const {projectId} =  req.params
+  const project = await Project.findById(req.params)
 
-const updateMemberRole = asyncHandler(async (req, res) => {});
+  if (!project) {
+    throw new ApiError(404, "Project not found");
+  }
+
+  const projectMembers = await ProjectMember.aggregate([
+    {
+      $match: {
+        project: new mongoose.Types.ObjectId(projectId)
+      }
+    },
+    {
+      $lookup : {
+        from: "users",
+        localField: "user",
+        foreignField: "_id",
+        as: "user",
+        pipeline: [
+          {
+            $project: {
+              _id: 1,
+              username: 1,
+              fullname: 1,
+              avatar : 1
+            }
+          }
+        ]
+      }
+    },
+    {
+      $addFields: {
+        user: {
+          $arrayElemAt: ["$user" , 0]
+        }
+      }
+    },
+    {
+      $project: {
+        project:1,
+        user: 1,
+        role: 1,
+        createdAt: 1,
+        updatedAt: 1,
+        _id: 0
+      }
+    }
+  ])
+
+  return res.status(200).json(new ApiResponse(200 , projectMembers , "Project members fetched"))
+});
+
+const updateMemberRole = asyncHandler(async (req, res) => {
+  
+});
 
 const deleteMember = asyncHandler(async (req, res) => {});
 
