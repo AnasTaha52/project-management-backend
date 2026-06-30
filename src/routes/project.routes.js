3d6d1b9826bff38eef816e261ab2dbd237fef760
AnasTaha52 <anastaha699@gmail.com>
2026-06-30 11:25:57 +0500

    write eouters for projects

--- src/routes/project.routes.js
+++ src/routes/project.routes.js
@@ -13,13 +13,45 @@ import {
 import { validate } from "../middlewares/validator.middleware.js";
 import {
   createProjectValidator,
-  addMemberToProjectValidator
+  addMemberToProjectValidator,
 } from "../validators/index.js";
 import {
   verifyJWT,
   validateProjectPermission,
 } from "../middlewares/auth.middleware.js";
+import { AvailableUserRole, UserRolesEnum } from "../utils/constants.js";
 
 const router = Router();
+router.use(verifyJWT);
+
+router
+  .route("/")
+  .get(getProjects)
+  .post(createProjectValidator(), validate, createProject);
+router
+  .route("/:projectId")
+  .get(validateProjectPermission(AvailableUserRole), getProjectById)
+  .put(
+    validateProjectPermission([UserRolesEnum.ADMIN]),
+    createProjectValidator(),
+    validate,
+    updateProject,
+  )
+  .delete(validateProjectPermission([UserRolesEnum.ADMIN]), deleteProject);
+
+router
+  .route("/:projectid/members")
+  .get(getProjectMembers)
+  .post(
+    validateProjectPermission([UserRolesEnum.ADMIN]),
+    addMemberToProjectValidator(),
+    validate,
+    addMembersToProject,
+  );
+
+router
+  .route("/:projectid/members/:userId")
+  .put(validateProjectPermission([UserRolesEnum.ADMIN]), updateMemberRole)
+  .delete(validateProjectPermission([UserRolesEnum.ADMIN]), deleteMember);
 
 export default router;
